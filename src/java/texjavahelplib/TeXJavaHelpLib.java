b2306a0590f53e08980418150adc329563acf7ac
nlct <n.talbot@uea.ac.uk>
2024-12-17 19:11:23 +0000

    updated version

--- src/java/texjavahelplib/TeXJavaHelpLib.java
+++ src/java/texjavahelplib/TeXJavaHelpLib.java
@@ -2381,6 +2381,6 @@ public class TeXJavaHelpLib
    public static final String MONO_CSS_CLASSES
     = ".code, .cmd, .cmdfmt, .csfmt, .csfmtfont, .csfmtcolourfont, .appfmt, .styfmt, .clsfmt, .envfmt, .optfmt, .csoptfmt, .styoptfmt, .clsoptfmt, .ctrfmt, .filefmt, .extfmt, .cbeg, .cend, .longargfmt, .shortargfmt, .qtt, .xmltagfmt, .varfmt, .terminal, .transcript, .filedef, .codebox, .badcodebox, .unicodebox, .compactcodebox, .sidebysidecode";
 
-   public static final String VERSION = "0.6a";
-   public static final String VERSION_DATE = "2024-08-28";
+   public static final String VERSION = "0.7a";
+   public static final String VERSION_DATE = "2024-12-17";
 }
