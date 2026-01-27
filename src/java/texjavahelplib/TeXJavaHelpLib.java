154d9daaac2e8b479ecfe0dee526e2bab05adc87
nlct <n.talbot@uea.ac.uk>
2026-01-27 20:18:55 +0000

    added linked images

--- src/java/texjavahelplib/TeXJavaHelpLib.java
+++ src/java/texjavahelplib/TeXJavaHelpLib.java
@@ -3934,6 +3934,6 @@ public class TeXJavaHelpLib
    public static final String LICENSE_GPL3 = String.format(
    "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>%nThis is free software: you are free to change and redistribute it.%nThere is NO WARRANTY, to the extent permitted by law.");
 
-   public static final String VERSION = "1.3";
-   public static final String VERSION_DATE = "2026-01-22";
+   public static final String VERSION = "1.3.20260127";
+   public static final String VERSION_DATE = "2026-01-27";
 }
