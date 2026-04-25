b82b4a09a64c40f8d6f870bb1f7bbd3dd84ef0ce
Nicola Talbot <n.talbot@uea.ac.uk>
2026-04-25 12:12:38 +0100

    updated version details

--- src/java/texjavahelplib/TeXJavaHelpLib.java
+++ src/java/texjavahelplib/TeXJavaHelpLib.java
@@ -3934,6 +3934,6 @@ public class TeXJavaHelpLib
    public static final String LICENSE_GPL3 = String.format(
    "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>%nThis is free software: you are free to change and redistribute it.%nThere is NO WARRANTY, to the extent permitted by law.");
 
-   public static final String VERSION = "1.3.20260419";
-   public static final String VERSION_DATE = "2026-04-19";
+   public static final String VERSION = "1.3";
+   public static final String VERSION_DATE = "2026-04-25";
 }
