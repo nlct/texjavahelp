eba88feaf232941db5f4b0f3f6a9650e9535e08f
Nicola Talbot <n.talbot@uea.ac.uk>
2026-05-19 22:34:53 +0100

    added unitfmt

--- src/java/texjavahelpmk/TeXJavaHelpSty.java
+++ src/java/texjavahelpmk/TeXJavaHelpSty.java
@@ -73,6 +73,7 @@ public class TeXJavaHelpSty extends UserGuideSty
       registerControlSequence(new VersionDate());
       registerControlSequence(new AtFirstOfOne("hologoRobust"));
       registerControlSequence(new AtFirstOfOne("hologo"));
+      registerControlSequence(new AtFirstOfOne("unitfmt"));
       registerControlSequence(new GenericCommand(true,
         "visiblespace", null, new TeXCsRef("textvisiblespace")));
 
