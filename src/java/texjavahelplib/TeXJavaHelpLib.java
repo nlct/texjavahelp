cffc1d1dfe807ad551fc833d1a1ec25fc0a77ec8
Nicola Talbot <n.talbot@uea.ac.uk>
2026-06-04 17:54:47 +0100

    added constructor

--- src/java/texjavahelplib/TeXJavaHelpLib.java
+++ src/java/texjavahelplib/TeXJavaHelpLib.java
@@ -119,6 +119,14 @@ public class TeXJavaHelpLib
    }
 
    public TeXJavaHelpLib(TeXJavaHelpLibApp application,
+       HelpSetLocale msgLocale, HelpSetLocale hsLocale)
+     throws IOException
+   {
+      this(application, application.getApplicationName(),
+            "", "", msgLocale, hsLocale);
+   }
+
+   public TeXJavaHelpLib(TeXJavaHelpLibApp application,
       String applicationName, String resourcebase,
       String dictionaryBase,
       HelpSetLocale messagesLocale, HelpSetLocale helpsetLocale)
@@ -4269,6 +4277,6 @@ public class TeXJavaHelpLib
    public static final String LICENSE_GPL3 = String.format(
    "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>%nThis is free software: you are free to change and redistribute it.%nThere is NO WARRANTY, to the extent permitted by law.");
 
-   public static final String VERSION = "1.3.20260601";
-   public static final String VERSION_DATE = "2026-06-01";
+   public static final String VERSION = "1.3.20260604";
+   public static final String VERSION_DATE = "2026-06-04";
 }
