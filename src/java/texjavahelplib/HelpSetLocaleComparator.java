24aa20df99163da0a99d76528fcb6d8c6b29124b
Nicola Talbot <n.talbot@uea.ac.uk>
2026-06-05 14:03:13 +0100

    Locale -> HelpSetLocale for helpset locale lists

---
+++ src/java/texjavahelplib/HelpSetLocaleComparator.java
@@ -0,0 +1,42 @@
+/*
+    Copyright (C) 2026 Nicola L.C. Talbot
+    www.dickimaw-books.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+package com.dickimawbooks.texjavahelplib;
+
+import java.util.Comparator;
+import java.util.Locale;
+
+/**
+ * A comparator that compares by language but ensures that more specific locales come first.
+ */
+
+public class HelpSetLocaleComparator implements Comparator<HelpSetLocale>
+{
+   public HelpSetLocaleLocaleComparator()
+   {
+      localeComparator = new LocaleComparator();
+   }
+
+   @Override
+   public int compare(HelpSetLocaleLocale hsl1, HelpSetLocaleLocale hsl2)
+   {
+      return localeComparator.compare(hsl1.getLocale(), hsl2.getLocale());
+   }
+
+   LocaleComparator localeComparator;
+}
